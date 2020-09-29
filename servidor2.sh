6f1517ad3b104457327c466c30962f6fa37dcff6
Askaredox <andyecarvajal@gmail.com>
2020-09-29 12:10:02 -0600

    perfeccion shell

--- servidor2.sh
+++ servidor2.sh
@@ -5,19 +5,19 @@
 
 # instalar Docker Engine
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 # instalar Docker Compose
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io
+sudo apt install -y docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # instalar Git
 sudo apt update
-sudo apt install git
+sudo apt install -y git
 
 # preparar
 git clone https://github.com/Askaredox/servidor-1-react.git
