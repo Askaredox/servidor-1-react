2312e2e08b7dab011af8d701470535864240c6e9
Askaredox <andyecarvajal@gmail.com>
2020-09-29 09:30:23 -0600

    modificacion de url

--- src/http-service/config.js
+++ src/http-service/config.js
@@ -1,4 +1,4 @@
 export class Config{
-    static servidor1='http://172.31.42.234:5000/api';
-    static servidor2='http://172.19.0.2:5000/api';
+    static servidor1='http://172.31.42.234:80/api';
+    static servidor2='http://172.19.0.2:80/api';
 }
